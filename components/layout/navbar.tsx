cab9783f078f4cfc913f4009681348871e977722
henriquecmelo1 <hcm@cin.ufpe.br>
2026-05-25 14:50:37 -0300

    feat: adjust logo on navbar and tab icon

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -15,7 +15,14 @@ export async function Navbar() {
     <header className="sticky top-0 z-50 border-b border-border-glow glass">
       <nav className="relative mx-auto flex h-16 w-full max-w-6xl items-center justify-between px-4">
         <Link href="/" className="text-xl font-semibold text-primary">
-          <Image src="/govscore.png" alt="Logo ScoreGov" height={35} width={70} />
+          <Image
+            src="/govscore.png"
+            alt="Logo ScoreGov"
+            width={0}
+            height={0}
+            sizes="100vw"
+            style={{ width: "120px", height: "auto" }}
+          />
         </Link>
 
         <NavbarMenu isAuthenticated={isAuthenticated} />
